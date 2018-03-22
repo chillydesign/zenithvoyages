b13a1280057170aec0be085e264967fb30a10771
Charles Harvey <harvey.charles@gmail.com>
2018-03-22 11:39:34 +0000

    use correct image in social_meta_properties

--- functions.php
+++ functions.php
@@ -574,7 +574,7 @@ function social_meta_properties(){
         $smp->description = $excerpt;
         $smp->image =  thumbnail_of_post_url( $post_id, 'large' );
         if ($smp->image == '') {
-            $smp->image =   get_template_directory_uri() . '/img/trans.png';
+        $smp->image =   get_template_directory_uri() . '/img/zenith-color.jpg';
         }
         $smp->url = get_the_permalink();
 
